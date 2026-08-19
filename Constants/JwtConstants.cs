6f0ad8d56f79443abb17d78a3a8ac8068cce028c
wach78 <dev@wach.nu>
2026-08-19 10:25:38 +0200

    fix

--- Constants/JwtConstants.cs
+++ Constants/JwtConstants.cs
@@ -1,5 +1,5 @@
 public static class JwtConstants
 {
-    public const int AccessTokenExpirationMinutes = 1;
+    public const int AccessTokenExpirationMinutes = 10;
     public const int RefreshTokenExpirationDays = 7;
 }
